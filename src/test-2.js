93f4006b875cfaf99e7d99c8b9217c8a41d76e75
Kayla Ajani <kaylaajani@Kaylas-MacBook-Air.local>
2025-11-26 16:31:40 +0700

    initial commit

---
+++ src/test-2.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const cp = require("child_process");
+
+const userInput = "2 + 2";
+eval(userInput);
+
+const pattern = process.argv[2];
+new RegExp(pattern);
+
+fs.readFileSync(process.argv[3]);
+
+cp.exec(userInput);
+
+const obj = {};
+obj[process.argv[4]] = true;
